32b76bc7489c81a0577a21786826fd1013898968
Paulo Bezerra <bezerra.paulo@gmail.com>
2022-08-29 17:48:50 -0400

    first commit

---
+++ src/test/java/br/com/coddev/cashflowapi/CashFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coddev.cashflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
